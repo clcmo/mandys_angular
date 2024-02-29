de8ad536e2e0d264c45ec8bfef56e4e87188123b
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2024-02-29 15:44:09 -0300

    Update template Html and Modules from MVVM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,40 +10,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
     RouterLink,
     RouterOutlet,
   ],
-  template: `
-    <main id="overlay">
-      <header class="brand">
-        <a [routerLink]="['/']" class="brand-name">
-          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-        </a>
-        <a class="link" href="#">About</a>
-        <a class="link" href="#">Contact</a>
-        <a class="link" href="#" id="turnoff">Acessibility</a>
-      </header>
-      <section class="content">
-        <router-outlet></router-outlet>
-      </section>
-      <footer>
-        <p>2024. Studio Urbanna + Komparti. Todos os direitos reservados.</p> 
-      </footer>
-    </main>
-    <script>
-      $(function () {
-        var lightsOff = false;
-        $('#turnoff').on('click', function () {
-            lightsOff = !lightsOff;
-            $('#overlay').fadeToggle(1000); /* Choose desired delay */
-            if (!lightsOff)
-                setTimeout((function() {
-                    $(this).removeClass('highlight');
-                }).bind(this), 1000); /* Same delay */
-            else
-                $(this).addClass('highlight');
-                $(this).removeId('overlay');
-        })
-      })
-    </script>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 
